0613b6bbef2901b11ab12ca5d65290c25bb1a763
Chris Gao <chrisgao64@gmail.com>
2022-11-05 17:31:50 -0400

    first commit

---
+++ modules/aws_subnet/main.tf
@@ -0,0 +1,7 @@
+#-------------------------AWS_Subnet Attached to AWS VPC-------------------
+resource "aws_subnet" "my_subnet" {
+    vpc_id = var.vpc_id
+    cidr_block = var.subnet_cidr
+
+    tags = var.subnet_name
+}
---
+++ modules/aws_vpc/main.tf
@@ -0,0 +1,9 @@
+#---------------------------------AWS_VPC----------------------------------
+resource "aws_vpc" "my_vpc" {
+    cidr_block = var.vpc_cidr
+
+    tags = var.vpc_name
+
+}
+
+
